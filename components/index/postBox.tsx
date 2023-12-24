3e764ec2d6ac13768b2bb17f92b78538e6b9e165
박지수 <jisupark2000@gmail.com>
2023-12-24 23:51:19 +0900

    Refactor: Change to Link tag

--- components/index/postBox.tsx
+++ components/index/postBox.tsx
@@ -12,27 +12,23 @@ export type PostInfoType = {
 export default function PostBox(post: PostInfoType) {
   const router = useRouter();
   return (
-    <article
-      onClick={() => router.push(`/posts/${post.path}`)}
-      className='v-box w-full px-30 py-20 max-w-[700px] cursor-pointer'
-    >
+    <Link href={`/posts/${post.path}`} className='v-box w-full px-30 py-20 max-w-[700px] cursor-pointer'>
       <h3 className='text-22 font-bold text-blue mb-10'>{post.title}</h3>
       <div className='text-14 mb-20'>{post.description}</div>
       <div className='flex-between'>
         <span className='text-14'>{post.publishedAt}</span>
         <div className='flex-center gap-3'>
           {post.keywords.map((keyword) => (
-            <button
+            <Link
               key={keyword}
-              // href={`/keywords/${keyword}`}
+              href={`/keywords/${keyword}`}
               className='bg-page1 text-blue py-3 px-10 rounded-20 text-14 hover:bg-page2'
-              onClick={() => router.push(`/keywords/${keyword}`)}
             >
               {keyword}
-            </button>
+            </Link>
           ))}
         </div>
       </div>
-    </article>
+    </Link>
   );
 }
